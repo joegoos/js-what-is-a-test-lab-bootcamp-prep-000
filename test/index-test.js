c1c8d6fc552db30b9ec13da39121515bc633227c
Joseph Guastamacchia <joeguasta@gmail.com>
2018-03-29 17:59:49 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,14 +1,14 @@
 var index = require("../index.js")
 
 describe('what-is-a-test', () => {
-  describe('Name', (Susan) => {
+  describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
     })
   })
 
 
-  describe('Height', (74) => {
+  describe('Height', () => {
     it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
     })
